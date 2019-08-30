a511f54c377685c42092243faffdb07a7ccda0af
orianagarcia <orugarcia@gmail.com>
2019-08-30 00:25:21 -0300

    Prueba de Subida tp por Git Bash

---
+++ prueba/prueba/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.cbxcoloc = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSumar.ForeColor = System.Drawing.Color.Indigo;
+            this.btnSumar.Location = new System.Drawing.Point(276, 243);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(116, 64);
+            this.btnSumar.TabIndex = 0;
+            this.btnSumar.Text = "Obtenga su presupuesto";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(249, 107);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(184, 20);
+            this.num1.TabIndex = 1;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(249, 166);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(184, 20);
+            this.num2.TabIndex = 2;
+            // 
+            // cbxcoloc
+            // 
+            this.cbxcoloc.AutoSize = true;
+            this.cbxcoloc.BackColor = System.Drawing.Color.LemonChiffon;
+            this.cbxcoloc.Location = new System.Drawing.Point(249, 205);
+            this.cbxcoloc.Name = "cbxcoloc";
+            this.cbxcoloc.Size = new System.Drawing.Size(78, 17);
+            this.cbxcoloc.TabIndex = 3;
+            this.cbxcoloc.Text = "colocacion";
+            this.cbxcoloc.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(149, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ALTO";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(149, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "ANCHO";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBox2.Location = new System.Drawing.Point(354, 205);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(79, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "solo placas";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.BackgroundImage = global::prueba.Properties.Resources.Gris2;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbxcoloc);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.btnSumar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.CheckBox cbxcoloc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ prueba/prueba/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        int multiplicar (int a, int b,int c)
+        {
+            return a * b *c;
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            if (cbxcoloc.ThreeState=true)
+            { 
+            MessageBox.Show(multiplicar(int.Parse(num1.Text), int.Parse(num2.Text),950).ToString());
+            }
+            if(checkBox2.ThreeState=true)
+            {
+                MessageBox.Show(multiplicar(int.Parse(num1.Text), int.Parse(num2.Text), 500).ToString());
+            }
+        }
+ 
+    }
+}
